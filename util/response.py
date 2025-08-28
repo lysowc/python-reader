481aa0e4fd174cdea88d8f483f0a8ae2df4a0273
lysowc <lysowc@gmail.com>
2025-08-28 20:18:12 +0800

    处理json异常

--- util/response.py
+++ util/response.py
@@ -1,10 +1,13 @@
 from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+
 
 def returnJson(data=None, message="Success", code=200, status_code=200):
     '''
     返回Json数据
     '''
+    res = jsonable_encoder(data)
     return JSONResponse(
         status_code=status_code,
-        content={"code": code, "message": message, "data": data},
+        content={"code": code, "message": message, "data": res},
     )
